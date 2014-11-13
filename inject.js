0d2fa0735cda129b698896318d3a76d6c19763d9
Nan <hunan1990@gmai.com>
2014-11-12 22:33:28 -0500

    check if buttons != undefined

--- inject.js
+++ inject.js
@@ -1,1 +1,3 @@
-$("#watch8-secondary-actions").append("<input type='button' name='button' id='button' value='Button'>");
+$buttons = $("#watch8-secondary-actions");
+if($buttons != "undefined")
+$buttons.append("<input type='button' name='button' id='button' value='Button1'>");
