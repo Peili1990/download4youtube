3addcc010a58d72df131cd1d049cc65510de76df
Nan <hunan1990@gmai.com>
2014-11-13 22:08:44 -0500

    add a icon to the button

--- inject.js
+++ inject.js
@@ -1,3 +1,5 @@
 $buttons = $("#watch8-secondary-actions");
-if($buttons != "undefined")
-$buttons.append("<button class='yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon action-panel-trigger yt-uix-videoactionmenu-button addto-button create-channel-lightbox  yt-uix-tooltip' type='button' onclick=';return false;' title='download'><span class='yt-uix-button-content'>download</span></button>");
+if($buttons != "undefined") {
+	var path = chrome.extension.getURL("download.png");
+	$buttons.append("<button class='yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon action-panel-trigger yt-uix-videoactionmenu-button addto-button create-channel-lightbox  yt-uix-tooltip' type='button' onclick=';return false;' title='download'><span class='yt-uix-button-icon-wrapper'><img src='" +  path + "'></span><span class='yt-uix-button-content'>download</span></button>");
+}
