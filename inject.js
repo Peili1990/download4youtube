fcf2053e2791a0df3a6705674ff6388fd3af57b6
pei <lipei3173@gmail.com>
2014-11-13 22:04:23 -0500

    no message

--- inject.js
+++ inject.js
@@ -1,3 +1,3 @@
 $buttons = $("#watch8-secondary-actions");
-if($buttons != "undefined")
-$buttons.append("<button class='yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon action-panel-trigger yt-uix-videoactionmenu-button addto-button create-channel-lightbox  yt-uix-tooltip' type='button' onclick=';return false;' title='download'><span class='yt-uix-button-content'>download</span></button>");
+if ($buttons != "undefined") 
+$buttons.append("<button class='yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon action-panel-trigger yt-uix-videoactionmenu-button addto-button create-channel-lightbox yt-uix-tooltip' type='button' onclick=';return false;' title='download' ><span class='yt-uix-button-content'>Download</span></button>");
