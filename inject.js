db13ca334dd7c13ed235be4f2e3ed1253efd364b
pei <lipei3173@gmail.com>
2014-11-17 00:26:02 -0500

    add judgment

--- inject.js
+++ inject.js
@@ -3,9 +3,11 @@ function myFunction() {
 }
 $buttons = $("#watch8-secondary-actions");
 if($buttons != "undefined") {
-	var path = chrome.extension.getURL("download.png");
-	$buttons.append("<button id='downloadbutton' class='yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon yt-uix-videoactionmenu-button addto-button yt-uix-tooltip' type='button' onclick=';return false;' title='download'><span class='yt-uix-button-icon-wrapper'><img src='" +  path + "'></span><span class='yt-uix-button-content'>download</span></button>");
-	$("#downloadbutton").click(function(e) {
+	if(window.location.href.substr(0,31)=="https://www.youtube.com/watch?v"){
+		 var path = chrome.extension.getURL("download.png");
+	     $("#watch8-secondary-actions").append("<button id='downloadbutton' class='yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon yt-uix-videoactionmenu-button addto-button yt-uix-tooltip' type='button' onclick=';return false;' title='download'><span class='yt-uix-button-icon-wrapper'><img src='" +  path + "'></span><span class='yt-uix-button-content'>download</span></button>");
+	     $("#downloadbutton").click(function(e) {
         myFunction()
     });
+   }
 }
