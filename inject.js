f84cf1f98e9ca65b76ca75d5c82ac89349fb35f8
nan <hunan1990@gmail.com>
2014-11-16 21:11:36 -0500

    add an action to the button

--- inject.js
+++ inject.js
@@ -1,5 +1,11 @@
+function myFunction() {
+	alert("11");
+}
 $buttons = $("#watch8-secondary-actions");
 if($buttons != "undefined") {
 	var path = chrome.extension.getURL("download.png");
-	$buttons.append("<button class='yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon action-panel-trigger yt-uix-videoactionmenu-button addto-button create-channel-lightbox  yt-uix-tooltip' type='button' onclick=';return false;' title='download'><span class='yt-uix-button-icon-wrapper'><img src='" +  path + "'></span><span class='yt-uix-button-content'>download</span></button>");
+	$buttons.append("<button id='downloadbutton' class='yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon yt-uix-videoactionmenu-button addto-button yt-uix-tooltip' type='button' onclick=';return false;' title='download'><span class='yt-uix-button-icon-wrapper'><img src='" +  path + "'></span><span class='yt-uix-button-content'>download</span></button>");
+	$("#downloadbutton").click(function(e) {
+        myFunction()
+    });
 }
